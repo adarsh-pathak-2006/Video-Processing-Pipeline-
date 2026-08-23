a9d20c938b1122b35642768ba88808dd0f62ea80
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-23 17:22:44 +0530

    added processing job model

--- workers/models.py
+++ workers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from core.models import Video
 
-# Create your models here.
+
+class ProcessinJOb(models.Model):
+    video=models.ForeignKey(Video, on_delete=models.CASCADE)
+    status=models.CharField(max_length=10, choices=[('PENDING', 'PENDING'), ('PROCESSING', 'PROCESSING'), ('COMPLETED', 'COMPLETED')], default='PENDING')
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_on=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.video.name
