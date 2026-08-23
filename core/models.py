9832140ed2ae98f6db06cb980677a55d48e56547
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-23 17:18:33 +0530

    added video modell

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    video=models.FileField(upload_to='uploaded_video/')
+    uploaded_on=models.DateTimeField(auto_now_add=True)
+    name=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
