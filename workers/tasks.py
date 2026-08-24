1e7f8ac4ead776b0190345a85ccf929a0d28b267
Adarsh <madhvetipathak2006@gmail.com>
2026-08-24 18:09:49 +0000

    removed task worker code

--- workers/tasks.py
+++ workers/tasks.py
@@ -3,12 +3,6 @@ from celery import shared_task
 from core.models import Video
 from .models import ProcessingJOb
 
-@shared_task
-def VideoProcessingQueue():
-    ProcessingJOb.status='PENDING'
-    ProcessingJOb.save()
-    time.sleep(10)
-    ProcessingJOb.status='COMPLETED'
-    ProcessingJOb.save()
+
 
 
