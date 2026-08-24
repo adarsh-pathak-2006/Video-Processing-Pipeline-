304cea31c08faa645d94c77642f19373d54165ff
Adarsh <madhvetipathak2006@gmail.com>
2026-08-24 18:03:16 +0000

    changed some settings in the views

--- core/views.py
+++ core/views.py
@@ -6,4 +6,6 @@ from rest_framework.generics import ListCreateAPIView
 class VideoUploadAPI(ListCreateAPIView):
     queryset=Video.objects.all()
     serializer_class=VideoUploadSerializer
-    
+
+    def perform_create(self, serializer):
+        serializer.save()
